15b8bb66d3d77614f8ff0a06a80cb2f4be22f3ea
fedemolina <federicoandresmolina@gmail.com>
2018-12-13 04:31:01 -0300

    plots

--- analisis_gallito_computrabajo.R
+++ analisis_gallito_computrabajo.R
@@ -59,23 +59,34 @@ saveRDS(ga, file = paste(ruta,'galito_2018-12-12',sep = ""))
 
 # Sigo el EDA (respecto a fecha)
 ga %>% ggplot(., aes(x = dia, y = ..count..)) +
-  geom_bar() +
-  labs(x = "", y = "cantidad de avisos", title = "Cantidad de avisos publicados en 'El Gallito' últimos 30 días")
+  geom_bar(aes(fill= ..count..)) +
+  labs(x = "", y = "cantidad de avisos", 
+       title = "Avisos laborales por día de la semana",
+       caption = "Cantidad de avisos laborales publicados en portal web 'El Galito' 12/Nov - 12/Dic 2018",
+       fill = "Cantidad\n avisos")
 
 ga %>% count(fpub) %>% ggplot(., aes(x=fpub, y = n)) +
   geom_line(color = "#FC4E07", size = 2) +
-  labs(x = 'fecha', y = 'cantidad de avisos', title = "Cantidad de vacantes, portal 'El galito' últimos 30 días")
+  labs(x = 'fecha de publicación', y = 'cantidad de avisos', 
+       title = "Avisos laborales por fecha de publicación",
+       caption = "Cantidad de avisos laborales publicados en portal web 'El Galito' 12/Nov - 12/Dic 2018. La fecha de publicación no es exacta")
 
 # Análisis de Área
 ga %>% ggplot(., aes(x = fct_infreq(f = area, ordered = TRUE))) +
   geom_bar(aes(fill = ..count..)) +
   labs(y = "cantidad de avisos", x = "Áreas", fill = "cantidad", 
-       title = "Cantidad de avisos publicados 'El Galito' últimos 30 días") +
+       title = "Áreas de actividad solicitadas",
+       caption = "Cantidad de avisos laborales publicados en 'El Galito' 12/Nov - 12/Dic 2018") +
   coord_flip() +
   geom_hline(yintercept = c(50,100,150), linetype = "dotted", 
              color = "red", size = 1)
 
 # Análisis de nivel
+ga %>% ggplot(., aes(nivel)) +
+  geom_bar()+
+  coord_polar(theta = "x",direction = 1, start = -50) +
+  labs(y = "Cantidad de avisos", x = "Nivel" , title = "Avisos laborales portal web 'El Gallito'",
+       caption = "Avisos laborales 'El Gallito' 12/Nov - 12/Dic 2018. Nivel técnico solicitado por empresas")
 
 # area y nivel
 ga  %>%  
@@ -84,6 +95,13 @@ ga  %>%
   coord_flip() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(y = "Nivel", x = "Área", title = "Nivel técnico y Área laboral", 
-       size = "cantidad\n avisos", color = "cantidad\n avisos")
+       size = "cantidad\n avisos", color = "cantidad\n avisos",
+       caption = "Avisos laborales publicados en portal web 'El Gallito' entre 12/Nov - 12/Dic de 2018. Área y nivel técnico solicitado")
+
+# dpto 
+table(ga$dpto)
+ga %>% ggplot(., aes(dpto)) +
+  geom_bar() +
+  coord_polar()
 
 #### 2. Análisis computrabajo ####
